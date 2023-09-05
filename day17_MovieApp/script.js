3eb58550a50481c5bfd3551361aa874be616ebcf
Jann <l33klin@foxmail.com>
2023-09-06 01:00:00 +0800

    day17: js

--- day17_MovieApp/script.js
+++ day17_MovieApp/script.js
@@ -3,6 +3,7 @@ const API_URL =
 const AUTH_TOKEN =
   "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZTdiMTQzMzlhNzM1M2ZlODA2ZTRjYWMzNGUyOTM4NCIsInN1YiI6IjY0YzM1OWViZDg2MWFmMDBmZmY5NGI4NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2qVULhQkJhYW_BgpfuRcD8qjnTmUQt3a-JYFq_wzkSA";
 const SEARCH_URL = "https://api.themoviedb.org/3/search/movie?query=";
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 const OPTIONS = {
   method: "GET",
   headers: {
@@ -11,16 +12,49 @@ const OPTIONS = {
   },
 };
 
+const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
+
+getMovies(API_URL);
 
 async function getMovies(url) {
   const resp = await fetch(url, OPTIONS);
   const data = await resp.json();
   console.log(data);
+  showMovies(data.results);
 }
 
-getMovies(API_URL);
+function showMovies(movies) {
+  main.innerHTML = "";
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie;
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
+
+    movieEl.innerHTML = `
+        <img src="${IMG_PATH + poster_path}" alt="${title}">
+        <div class="movie-info">
+      <h3>${title}</h3>
+      <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+        </div>
+        <div class="overview">
+      <h3>Overview</h3>
+      ${overview}
+    </div>`;
+    main.appendChild(movieEl);
+  });
+}
+
+function getClassByRate(vote_average) {
+  if (vote_average >= 8) {
+    return "green";
+  } else if (vote_average >= 6) {
+    return "orange";
+  } else {
+    return "red";
+  }
+}
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
