092b3505ec6d8d9c7539d82e3043fc42a58c8d54
Jann <l33klin@foxmail.com>
2023-09-09 23:52:40 +0800

    day19: add js

--- day19_ThemeClock/script.js
+++ day19_ThemeClock/script.js
@@ -1,6 +1,7 @@
 const hourEl = document.querySelector(".hour");
 const minuteEl = document.querySelector(".minute");
 const secondEl = document.querySelector(".second");
+const timeEl = document.querySelector(".time");
 const dateEl = document.querySelector(".date");
 const toggleEl = document.querySelector(".toggle");
 
@@ -39,3 +40,48 @@ toggleEl.addEventListener("click", (e) => {
     e.target.innerHTML = "Light mode";
   }
 });
+
+function setTime() {
+  const time = new Date();
+  const month = time.getMonth();
+  const day = time.getDay();
+  const hours = time.getHours();
+  const hoursForClock = hours % 12;
+  const minutes = time.getMinutes();
+  const seconds = time.getSeconds();
+
+  hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    hoursForClock,
+    0,
+    12,
+    0,
+    360
+  )}deg)`;
+  minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    minutes,
+    0,
+    60,
+    0,
+    360
+  )}deg)`;
+  secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    seconds,
+    0,
+    60,
+    0,
+    360
+  )}deg)`;
+
+  timeEl.innerText = `${hours}:${minutes < 10 ? "0" + minutes : minutes}`;
+}
+
+//https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+function scale(number, inMin, inMax, outMin, outMax) {
+  return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+}
+
+setTime();
+
+setInterval(() => {
+  setTime();
+}, 1000);
