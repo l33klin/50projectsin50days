c82b07494bf0ffd68b4f6cbdef8b43fa2a81c4a1
Jann <l33klin@foxmail.com>
2023-09-23 18:45:34 +0800

    day40

---
+++ day40_3DBoxesBackground/script.js
@@ -0,0 +1,19 @@
+const boxes = document.getElementById("boxes");
+const magicBtn = document.getElementById("btn");
+
+magicBtn.addEventListener("click", function (e) {
+  boxes.classList.toggle("big");
+});
+
+function createBoxes() {
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      const box = document.createElement("div");
+      box.classList.add("box");
+      box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`;
+      boxes.appendChild(box);
+    }
+  }
+}
+
+createBoxes();
