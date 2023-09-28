7509baca1e188cdc96378e03fcde0dde66f5334b
Jann <l33klin@foxmail.com>
2023-09-29 00:36:24 +0800

    day50: add js

--- day50_InsectCatchGame/script.js
+++ day50_InsectCatchGame/script.js
@@ -1,3 +1,6 @@
 const screens = document.querySelectorAll(".screen");
+const chooseObjBtns = document.querySelectorAll(".choose-obj-btn");
+const startBtn = document.getElementById("start-btn");
+const gameContainer = document.querySelector(".game-container");
 
-screens[0].classList.add("up");
+// screens[0].classList.add("up");
